c10729124c3799330743a072e98a05a9d0decb56
MiliBrime <milibrime@hotmail.com>
2024-04-05 18:59:06 -0300

    borre codigo que no se usaba

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,13 +1,10 @@
 import React,{Component} from "react";
-import {useParams} from "react-router-dom";
 
 import ContentWrapperUsers from './ContentWrapperUsers';
 import SideBar from "./SideBar";
 
 class Users extends Component {
     render(){
-            let { id } = useParams();
-            let idReal = Number( id );
         return(
            <>
            <SideBar/>
